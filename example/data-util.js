bd6622965ce6f54300f943259d4db5736f89066d
russa <aaron.russ@dfki.de>
2017-01-14 00:42:15 +0100

    FIX export wav with header

--- example/data-util.js
+++ example/data-util.js
@@ -36,7 +36,7 @@ function exportUI8ArrayBuffer(recBuffers, recLength){
 /**
  *  creates blob element from libflac-decoder output
  */
-function exportFile(recBuffers){
+function exportFile(recBuffers, sampleRate){
 	//get length
 	var recLength = 0;
 	for(var i=recBuffers.length - 1; i >= 0; --i){
@@ -45,9 +45,59 @@ function exportFile(recBuffers){
 	
 	//convert buffers into one single buffer
 	var samples = exportUI8ArrayBuffer(recBuffers, recLength);
-	var the_blob = new Blob([samples]);
+	var dataView = encodeWAV(samples, sampleRate, 1/*mono*/);
+	var the_blob = new Blob([dataView], {type: 'audio/wav'});
 	return the_blob;
-}        
+}
+
+function encodeWAV(samples, sampleRate, channels){
+	
+	var buffer = new ArrayBuffer(44 + samples.length);
+	var view = new DataView(buffer);
+
+	/* RIFF identifier */
+	writeString(view, 0, 'RIFF');
+	/* file length */
+	view.setUint32(4, 32 + samples.length * 2, true);
+	/* RIFF type */
+	writeString(view, 8, 'WAVE');
+	/* format chunk identifier */
+	writeString(view, 12, 'fmt ');
+	/* format chunk length */
+	view.setUint32(16, 16, true);
+	/* sample format (raw) */
+	view.setUint16(20, 1, true);
+	/* channel count */
+	view.setUint16(22, channels, true);
+	/* sample rate */
+	view.setUint32(24, sampleRate, true);
+	/* byte rate (sample rate * block align) */
+	view.setUint32(28, sampleRate * channels * 2, true);
+	/* block align (channel count * bytes per sample) */
+	view.setUint16(32, channels * 2, true);
+	/* bits per sample */
+	view.setUint16(34, 16, true);
+	/* data chunk identifier */
+	writeString(view, 36, 'data');
+	/* data chunk length */
+	view.setUint32(40, samples.length * 2, true);
+
+	writeData(view, 44, samples)
+
+	return view;
+}
+
+function writeString(view, offset, string) {
+	for (var i = 0; i < string.length; i++) {
+		view.setUint8(offset + i, string.charCodeAt(i));
+	}
+}
+
+function writeData(output, offset, input){
+	for (var i = 0; i < input.length; ++i, ++offset){
+		output.setUint8(offset, input[i], true);
+	}
+}
 
 /**
  *  create A-element for data BLOB and trigger download
