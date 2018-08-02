9bb664f73a575d7dfd636b9add79cd5bbe2dcfd9
Radium Zheng <ztl8702@msn.com>
2018-08-02 14:30:18 +1000

    use a placeholder for npm package entrypoint

---
+++ index.js
@@ -0,0 +1,5 @@
+// Entry point placeholder
+
+console.warn('Looks like you are trying to import libflac.js as a Node module. '
+ + 'libflac.js does not currently support such usage. Please refer to '
+ + 'https://github.com/mmig/libflac.js for how to import libflac.js.')
