3513f9664cd9831b12c45e36b2fb2d3a60d2d127
russa <aaron.russ@dfki.de>
2020-06-20 02:14:20 +0200

    improved typings
    do export type and interface definitions

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,44 @@
 
 import * as LibFlac from './dist/index.d';
 
+import type {
+	CreatedEvent, DestroyedEvent, CoderChangedEventData, ReadyEvent,
+	BlockMetadata, Metadata, ReadResult,  StreamMetadata, CodingOptions,
+	decoder_error_callback_fn, decoder_read_callback_fn, decoder_write_callback_fn,
+	encoder_write_callback_fn, metadata_callback_fn,
+	FLAC__StreamDecoderErrorStatus, FLAC__StreamDecoderInitStatus, FLAC__StreamDecoderState,
+	FLAC__StreamEncoderInitStatus, FLAC__StreamEncoderState,
+	SubFrameMetadata, SubFramePartition, SubFramePartitionContent, SubFramePartitionData, FixedSubFrameData,
+	FLAC__SubframeType, FLAC__ChannelAssignment, FLAC__EntropyCodingMethodType, LPCSubFrameData
+} from './dist/index.d';
+
 export = libFactory;
 
-declare function libFactory(libVariant?: string): libFactory.Flac;
+interface FlacVariant {
+	/**
+	 * indicates which variant of the library instance:
+	 * <pre>["release" | "dev" | "min"] + "." + ["asmjs" | "wasm"]</pre>
+	 * for example
+	 * <pre>"release.wasm"</pre>
+	 *
+	 * NOTE this property is only available when the library instance was
+	 *      created by the (Node.js) factory method
+	 */
+	variant: string;
+}
+
+declare function libFactory(libVariant?: string): libFactory.Flac & FlacVariant;
 
 declare namespace libFactory {
-	export type Flac = typeof LibFlac;
+	export type Flac = typeof LibFlac & FlacVariant;
+	export {
+		CreatedEvent, DestroyedEvent, CoderChangedEventData, ReadyEvent,
+		BlockMetadata, Metadata, ReadResult,  StreamMetadata, CodingOptions,
+		decoder_error_callback_fn, decoder_read_callback_fn, decoder_write_callback_fn,
+		encoder_write_callback_fn, metadata_callback_fn,
+		FLAC__StreamDecoderErrorStatus, FLAC__StreamDecoderInitStatus, FLAC__StreamDecoderState,
+		FLAC__StreamEncoderInitStatus, FLAC__StreamEncoderState,
+		SubFrameMetadata, SubFramePartition, SubFramePartitionContent, SubFramePartitionData, FixedSubFrameData,
+		FLAC__SubframeType, FLAC__ChannelAssignment, FLAC__EntropyCodingMethodType, LPCSubFrameData
+	}
 }
