207d7424d3a03774928c1abf30c285e6f947ee3d
russa <aaron.russ@dfki.de>
2020-07-10 22:54:21 +0200

    do sort entries for generated EXPORTED_FUNCTIONS string

--- tools/extract_EXPORTED_FUNCTIONS.js
+++ tools/extract_EXPORTED_FUNCTIONS.js
@@ -1,4 +1,8 @@
 
+/**
+ * HELPER script for extracting exported C functions
+ * (to be used for emscripten compiler setting EXPORTED_FUNCTIONS)
+ */
 
 var fs = require('fs'),
 	path = require('path');
@@ -13,14 +17,15 @@ while(res = re.exec(str)){
 	funcs.add(res[4]);
 }
 
-var sb = ["-s EXPORTED_FUNCTIONS='["];
-for(var f of funcs){
-	sb.push('"_', f, '"', ',');
-}
+var list = Array.from(funcs).sort(function(a, b){
+	return a.localeCompare(b);
+});
 
-if(sb.length > 1){
-	sb.splice(sb.length - 1, 1);
-}
+var len = list.length;
+var sb = list.map(function(f, i){
+	return '"_' + f + '"' + (i === len -1? '' : ',');
+});
+sb.unshift("-s EXPORTED_FUNCTIONS='[");
 sb.push("]'");
 
 console.log(sb.join(''));
